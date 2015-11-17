551d89ebef69d3aa2ecb294b38d37a29f0c19dfa
Eric OC <eric@ericoc.com>
2015-11-16 20:35:54 -0500

    inital import

---
+++ README.md
@@ -0,0 +1,52 @@
+# About
+This is still under construction, but I am trying to make a re-usable PHP library for the Philadelphia Indego Bike Share API!
+
+Maybe check out https://github.com/CityOfPhiladelphia while you are waiting for my library to be more usable!
+
+## Example output
+
+	$ php indego.class.php
+	stdClass Object
+	(
+	    [addressStreet] => 1076 Berks Street
+	    [addressCity] => Philadelphia
+	    [addressState] => PA
+	    [addressZipCode] => 19122
+	    [bikesAvailable] => 5
+	    [closeTime] => 23:58:00
+	    [docksAvailable] => 12
+	    [eventEnd] =>
+	    [eventStart] =>
+	    [isEventBased] =>
+	    [isVirtual] =>
+	    [kioskId] => 3008
+	    [kioskPublicStatus] => Active
+	    [name] => Temple University Station
+	    [openTime] => 00:02:00
+	    [publicText] =>
+	    [timeZone] => Eastern Standard Time
+	    [totalDocks] => 19
+	    [trikesAvailable] => 0
+	)
+	stdClass Object
+	(
+	    [addressStreet] => 3051 South St.
+	    [addressCity] => Philadelphia
+	    [addressState] => PA
+	    [addressZipCode] => 19147
+	    [bikesAvailable] => 0
+	    [closeTime] => 23:58:00
+	    [docksAvailable] => 35
+	    [eventEnd] =>
+	    [eventStart] =>
+	    [isEventBased] =>
+	    [isVirtual] =>
+	    [kioskId] => 3020
+	    [kioskPublicStatus] => Active
+	    [name] => University City Station
+	    [openTime] => 00:02:00
+	    [publicText] =>
+	    [timeZone] => Eastern Standard Time
+	    [totalDocks] => 35
+	    [trikesAvailable] => 0
+	)
---
+++ indego.class.php
@@ -0,0 +1,101 @@
+<?php
+
+// Create a class to work with the Philadelphia Indego Bike Share API
+class Indego {
+
+	// Create empty private array to fill in with station data from our constructor
+	private $stations = [];
+
+	// Hit the bike-share API and find all of the stations upon instantiation of the class
+	public function __construct() {
+
+		// Specify the Indego bikes API URL
+		$url = 'https://api.phila.gov/bike-share-stations/v1';
+
+		// Specify a friendly user-agent to hit the API with
+		$user_agent = 'Indego PHP API Library - https://github.com/ericoc/indego-php-lib';
+
+		// Hit the API to get the JSON response
+		$c = curl_init();
+		curl_setopt($c, CURLOPT_URL, $url);
+		curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($c, CURLOPT_USERAGENT, $user_agent);
+		curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 2);
+		curl_setopt($c, CURLOPT_TIMEOUT, 2);
+		$r = curl_exec($c);
+		curl_close($c);
+
+		// Decode the JSON response from the API
+		$raw = json_decode($r);
+
+		// Add each station to our own array that's easier to work with
+		foreach ($raw->features as $station) {
+			$this->addStation($station->properties);
+		}
+	}
+
+	// Create a function to add stations to our own array (from large passed in array)
+	private function addStation($properties) {
+		$id = $properties->kioskId;		//	Get the station kiosk ID
+		$this->stations[$id] = new stdClass();	//	Make a new object in our own array for the station
+
+		// Fill in the properties of the station in to our own array
+		foreach ($properties as $name => $value) {
+			$this->stations[$id]->$name = $value;
+		}
+	}
+
+	// Create a function to search for and return stations
+	public function getStations($where = '') {
+
+		// Just provide all of the stations if no search query was given
+		if (empty($where)) {
+			$return = $this->stations;
+
+		// If a search query was passed, process it...
+		} else {
+
+			// Create a case-insensitive pattern to match station names and addresses with
+			$pattern = '/' . trim($where) . '/i';
+
+			// Loop through each station in the primary array
+			foreach($this->stations as $station) {
+
+				// If the search query is five digits, only match the stations with that zip code
+				if ( (is_numeric($where)) && (strlen($_GET['where']) == 5) ) {
+					if ($station->addressZipCode == $where) {
+						$return[$station->kioskId] = $station;
+					}
+
+				// Do a regular expression match using the search query on the name and address of each station
+				} elseif ( (preg_match($pattern, $station->addressStreet)) || (preg_match($pattern, $station->name)) ) {
+					$return[$station->kioskId] = $station;
+				}
+			}
+		}
+
+		// Return the stations!
+		return $return;
+	}
+
+	// Nothing in the destructor for now, but maybe some day!
+	public function __destruct() {
+		// nothing yet
+	}
+}
+
+error_reporting(E_ALL);
+
+// Instantiate the Indego class which will instantly discover all of the stations using the constructor
+$indego = new Indego;
+
+// Get all of the stations
+//$stations = $indego->getStations();
+//var_dump($stations);
+
+// Just get "university" stations
+$uni_stations = $indego->getStations('university');
+
+foreach ($uni_stations as $uni_station) {
+	print_r($uni_station);
+}
